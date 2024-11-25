be080dc40f5487f4b4abc1a43d7b76cb706089c6
chai <chaitanyagupta759@gmail.com>
2024-11-25 15:19:32 +0530

    removed package.json

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -69,6 +69,8 @@ const Post = ({ post, setCurrentId }) => {
         <div className={classes.overlay2} name="edit">
           <Button
             onClick={(e) => {
+         
+         
               e.stopPropagation();
               setCurrentId(post._id);
             }}
